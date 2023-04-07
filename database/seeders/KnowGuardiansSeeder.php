16fb5dbf3217abceeaff9572b9f9ce6d39c29321
zarrok <anakinvv@gmail.com>
2023-04-07 11:16:45 -0500

    KGseeder

--- database/seeders/KnowGuardiansSeeder.php
+++ database/seeders/KnowGuardiansSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\KnowGuardians;
+use Doctrine\DBAL\Driver\IBMDB2\Exception\Factory;
 
 class KnowGuardiansSeeder extends Seeder
 {
@@ -16,13 +17,9 @@ class KnowGuardiansSeeder extends Seeder
     public function run()
     {
         // Crea 50 registros aleatorios en la tabla know_guardians
-        for ($i = 0; $i < 50; $i++) {
-            $knowGuardian = new KnowGuardians();
-            $knowGuardian->id_guardian = rand(1, 20); // assuming there are 20 guardians
-            $knowGuardian->id_beneficiary = rand(1, 50); // assuming there are 50 beneficiaries
-            $knowGuardian->know_needs = rand(0, 1);
-            $knowGuardian->concept = rand(1, 10);
-            $knowGuardian->save();
+        for ($i = 0; $i < 10; $i++) {
+            // Create 10 KnowGuardians records
+            Factory(KnowGuardian::class, 10)->create();
         }
     }
 }
