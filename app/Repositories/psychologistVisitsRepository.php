9a9c52906cc9a38dd4390eccf13b29f9d809f577
zarrok <anakinvv@gmail.com>
2023-04-18 23:12:40 -0500

    repositorio

--- app/Repositories/psychologistVisitsRepository.php
+++ app/Repositories/psychologistVisitsRepository.php
@@ -3,8 +3,8 @@
 namespace App\Repositories;
 
 use App\Http\Resources\V1\BeneficiaryResource;
-use App\Http\Resources\V1\VisitSubDirectorCollection;
-use App\Http\Resources\V1\VisitSubDirectorResource;
+use App\Http\Resources\V1\PsychologistVisitsCollection;
+use App\Http\Resources\V1\PsychologistVisitsResource;
 use App\Models\psychologistVisits;
 use App\Models\Beneficiary;
 use App\Traits\FunctionGeneralTrait;
@@ -25,7 +25,40 @@ class CustomVisitRepository
 
     public function getAll()
     {
-   
+        $rol_id = $this->getIdRolUserAuth();
+        $user_id = $this->getIdUserAuth();
+
+        $query = $this->model->query()->orderBy('id', 'DESC');
+
+    
+
+        switch ($rol_id) {
+            case config('roles.coordinador_psicosocial'):
+                $query = $query->whereNotIn('created_by', [1,2])->whereHas('createdBy.roles', function ($query) {
+                    $query->where('roles.slug', 'psicologo');
+                })->whereNotIn('status_id', [config('status.APR')]);
+                break;
+
+            case config('roles.psicologo'):
+                $query->where('created_by', $user_id)
+                ->whereNotIn('status_id', [config('status.APR')]);
+                break;
+
+            default:
+                return null;
+                break;
+        }
+
+        $paginate = config('global.paginate');
+
+        // Aplicar filtros adicionales desde la URL
+        $query = $this->model->scopeFilterByUrl($query);
+
+        // Calcular número de páginas para paginación
+        session()->forget('count_page_custom_visits');
+        session()->put('count_page_custom_visits', ceil($query->count()/$paginate));
+
+        return new PsychologistVisitsCollection($query->simplePaginate($paginate));
     }
     public function create($request)
     {
@@ -54,6 +87,35 @@ class CustomVisitRepository
     }
 
     public function getValidate($data, $method){
+        
+        $validate = [
+            'theme' => 'bail|required',
+            'agreements' => 'bail|required',
+            'concept' => 'bail|required',
+            'guardian_knows_semilleros' => 'bail|required',
+            'month' => 'bail|required',
+            'beneficiary' => 'bail|required',
+            'municipality' => 'bail|required',
+            'file' => $method != 'update' ? 'bail|required|mimes:application/pdf,pdf,png,webp,jpg,jpeg|max:' . (500 * 1049000) : 'bail',
+        ];
+
+        $messages = [
+            'required' => ':attribute es obligatorio.',
+            'max' => ':attribute es muy grande.',
+        ];
+
+        $attrs = [
+            'theme' => 'Tema',
+            'agreements' => 'Acuerdos y recomendaciones',
+            'concept' => 'Concepto',
+            'guardian_knows_semilleros' => '¿El padre o acudiente conoce el proyecto de Semilleros Deportivos?',
+            'month' => 'Cumple con el desarrollo tecnico del mes',
+            'beneficiary' => 'Beneficiario',
+            'file' => 'Archivo',
+            'municipality' => 'Municipio',
+        ];
+
+        return $this->validator($data, $validate, $messages, $attrs);
 
     }
 
