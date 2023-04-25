be8353cc46f6f26a76a4bec6675b05705da3a9fe
zarrok <anakinvv@gmail.com>
2023-04-25 14:39:24 -0500

    Migraciones de reportes usuario

---
+++ database/migrations/2023_04_25_124008_update_get_report_methodologist.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $views = "
+        DROP VIEW get_report_methodologist;
+        CREATE VIEW get_report_methodologist AS
+            SELECT users.name, users.lastname, users.document_number, users.gender,count(methodologist_visits.id) as visits, users.created_at
+            FROM users
+            JOIN role_user ON users.id = role_user.user_id
+            JOIN roles ON role_user.role_id = roles.id
+            INNER JOIN methodologist_visits ON roles.id = methodologist_visits.created_by
+            WHERE roles.id = 17
+            GROUP BY users.id,users.name, users.lastname, users.document_number, users.gender,users.created_at;
+        ";
+        DB::unprepared($views);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $views= "DROP VIEW get_report_methodologist;";
+        DB::unprepared($views);
+
+    }
+};
---
+++ database/migrations/2023_04_25_124136_update_get_report_psicosocial.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $views = "
+        DROP VIEW get_report_psicosocial;
+        CREATE VIEW get_report_psicosocial AS
+            SELECT users.name, users.lastname, users.document_number, users.gender,COUNT(custom_visits.id) AS custom_visits,COUNT(psychological_visits.id) AS visits,COUNT(transversal_activities.id) AS transversal_activities, users.created_at
+            FROM users
+            JOIN role_user ON users.id = role_user.user_id
+            JOIN roles ON role_user.role_id = roles.id
+            INNER JOIN custom_visits ON custom_visits.created_by = users.id 
+            INNER JOIN psychological_visits ON psychological_visits.created_by = users.id 
+            INNER JOIN transversal_activities ON transversal_activities.created_by = users.id 
+            WHERE roles.id = 18
+            GROUP BY users.id,users.name, users.lastname, users.document_number, users.gender,users.created_at;
+        ";
+        DB::unprepared($views);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $views= "DROP VIEW get_report_psicosocial;";
+        DB::unprepared($views);
+
+    }
+};
