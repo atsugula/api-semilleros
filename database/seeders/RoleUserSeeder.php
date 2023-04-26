d8b767a9eb146e3e04c3a034f2a3f3c9500c10c5
ATSU <atsgula@gmail.com>
2023-04-26 15:00:45 -0500

    asignacion de roles a usuarios de prueba

--- database/seeders/RoleUserSeeder.php
+++ database/seeders/RoleUserSeeder.php
@@ -16,28 +16,22 @@ class RoleUserSeeder extends Seeder
     public function run()
     {
         DB::table('role_user')->insert([
-            // ['role_id' => 1, 'user_id' => 1],
-            // ['role_id' => 2, 'user_id' => 2],
+            ['role_id' => 1, 'user_id' => 1],
             /* SUBDIRECTOR */
-            // ['role_id' => 4, 'user_id' => 4],
+            ['role_id' => 4, 'user_id' => 4],
             /* DIRECTORES */
-            // ['role_id' => 3, 'user_id' => 3],
+            ['role_id' => 2, 'user_id' => 2],
+            ['role_id' => 3, 'user_id' => 3],
             /* COORDINADORES */
-            /* ['role_id' => 6, 'user_id' => 6],
-            ['role_id' => 15, 'user_id' => 15],
-            ['role_id' => 16, 'user_id' => 16], */
-            /* APOYO */
-            /* ['role_id' => 10, 'user_id' => 10],
-            ['role_id' => 12, 'user_id' => 12], */
-            /* GENERAL */
-            /* ['role_id' => 8, 'user_id' => 8],
+            ['role_id' => 5, 'user_id' => 5],
+            ['role_id' => 6, 'user_id' => 6],
             ['role_id' => 9, 'user_id' => 9],
+            /* GENERAL */
+            ['role_id' => 7, 'user_id' => 7],
+            ['role_id' => 8, 'user_id' => 8],
+            ['role_id' => 10, 'user_id' => 10],
             ['role_id' => 11, 'user_id' => 11],
-            ['role_id' => 13, 'user_id' => 13],
-            ['role_id' => 14, 'user_id' => 14],
-            ['role_id' => 17, 'user_id' => 17],
-            ['role_id' => 18, 'user_id' => 18],
-            ['role_id' => 19, 'user_id' => 19], */
+            ['role_id' => 12, 'user_id' => 12],
         ]);
     }
 }
