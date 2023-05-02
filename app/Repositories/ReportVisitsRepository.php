ad9fef33baedc49dd3c225b6695e2db7c1edbc22
zarrok <anakinvv@gmail.com>
2023-05-02 15:26:59 -0500

    Word generate 1

---
+++ app/Repositories/ReportVisitsRepository.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Repositories;
+
+use PhpOffice\PhpWord\TemplateProcessor;
+use PhpOffice\PhpWord\PhpWord;
+
+
+class ReportVisitsRepository
+{ 
+
+  public function generateDoc(){
+    $templateProcessor = new TemplateProcessor(asset('Template\metodologo\Metodologist_Visit.docx'));
+     
+    $data = [
+      'nombre' => 'Juan',
+    ];
+    $templateProcessor->setValues($data);
+    $templateProcessor->saveAs('Template\metodologo\Metodologist_Visit.docx');
+
+  }
+
+}
