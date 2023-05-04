0f073627118bad20781abc639d4e7ad47ab798a8
zarrok <anakinvv@gmail.com>
2023-05-04 11:17:55 -0500

    quitandoControllvisit

--- app/Http/Controllers/V1/ReportMethodologistVisitController.php
+++ app/Http/Controllers/V1/ReportMethodologistVisitController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Repositories\ReportRepository;
 
 class ReportMethodologistVisitController extends Controller
 {
