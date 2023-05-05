dee7124f2c5695bd523c8c4a726e93aac2ccf8d4
Jonathan Lopez <jlopez0313@hotmail.com>
2023-05-05 09:56:58 -0500

    bug al consultar beneficiarios y cronogramas en rol monitor

--- app/Repositories/ChronogramRepository.php
+++ app/Repositories/ChronogramRepository.php
@@ -32,10 +32,10 @@ class ChronogramRepository
         $user_id = $this->getIdUserAuth();
 
         $query = $this->model->query()->orderBy('id', 'DESC');
-
-        if ($rol_id == config('roles.monitor')) {
+        
+        if ($rol_id == config('roles.monitor')) {            
             $query->where('created_by', $user_id)
-                ->whereNotIn('status_id', config('roles.APR'));
+            ->whereNotIn('status_id', [config('roles.APR')]);
         }
 
         if ($rol_id == config('roles.coordinador_psicosocial') || $rol_id == config('roles.coordinador_regional') || $rol_id == config('roles.coordinador_enlace')) {
@@ -43,7 +43,7 @@ class ChronogramRepository
                 ->whereHas('creator.roles', function ($query) {
                     $query->where('roles.slug', 'subdirector_tecnico');
                 })
-                ->whereNotIn('status_id', config('roles.APR'));
+                ->whereNotIn('status_id', [config('roles.APR')]);
         }
 
         $paginate = config('global.paginate');
