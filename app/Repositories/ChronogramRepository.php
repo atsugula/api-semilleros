38ec47e16a8781af9ab3f1666b43aeacb4fa7618
Jesus-0sorio <jaoz10.16@gmail.com>
2023-05-09 11:20:28 -0500

    Muestra los cronogromas con rol monitor

--- app/Repositories/ChronogramRepository.php
+++ app/Repositories/ChronogramRepository.php
@@ -34,8 +34,7 @@ class ChronogramRepository
         $query = $this->model->query()->orderBy('id', 'DESC');
         
         if ($rol_id == config('roles.monitor')) {            
-            $query->where('created_by', $user_id)
-            ->whereNotIn('status_id', [config('roles.APR')]);
+            $query->where('created_by', $user_id);
         }
 
         if ($rol_id == config('roles.coordinador_psicosocial') || $rol_id == config('roles.coordinador_regional') || $rol_id == config('roles.coordinador_enlace')) {
