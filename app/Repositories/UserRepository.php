9f1b0a3e74468510a9bc3928924f71bfb6a4ef9b
zarrok <anakinvv@gmail.com>
2023-05-17 22:19:06 -0500

    Update UserRepository.php

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -37,13 +37,13 @@ class UserRepository
 
         $rol_id = $this->getIdRolUserAuth();
 
-        if ($rol_id == config('roles.root')){
+        if ($rol_id == config('roles.super_root')){
             $query->whereHas('roles', function ($profile) {
                 $profile->whereNotIn('roles.id', [config('roles.super_root')]);
             });
         }
 
-        return  new UserCollection($query->paginate(25));
+        return  new UserCollection($query->paginate(200));
 
     }
 
