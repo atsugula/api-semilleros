66a25163ca08f0178f93d7f25ecb7a4eaaee8a94
zarrok <anakinvv@gmail.com>
2023-05-27 17:03:43 -0500

    Update ReportChronogramRepository.php

--- app/Repositories/ReportChronogramRepository.php
+++ app/Repositories/ReportChronogramRepository.php
@@ -10,66 +10,75 @@ use Illuminate\Support\Facades\Response;
 use PhpOffice\PhpWord\PhpWord;
 
 class ReportChronogramRepository
-{
-    private $cronogram;
+{ 
+  private $cronogram;
 
-    public function __construct()
-    {
-        $this->cronogram = new Chronogram();
+  function __construct()
+  {
+    $this->cronogram = new Chronogram();
+  }
+
+  public function generateDoc($id)
+  {
+    $ChronogramReport = $this->cronogram->findOrFail($id);
+    $grupo = $ChronogramReport->load('groups');
+    
+    $templatePath = public_path('Template/Chronogram/template');
+    $outputPath = public_path('Template/Chronogram/template/Chronograma_'. $id .'.docx');
+
+    $templateProcessor = new TemplateProcessor($templatePath);
+     
+    $data = [
+        'update_date' => $grupo->updated_at,
+        'MONTH' => $grupo->mes->name,
+        'MONITOR_name' => $grupo->creator->name,
+        'MONITOR_lastname' => $grupo->creator->lastname,
+        'zone' => $grupo->zone->name,
+        'municipio' => $grupo->municipio->name,
+    ];
+
+    $templateProcessor->setValues($data);
+
+    $groups = $grupo->groups;
+    $tableData = [];
+    foreach ($groups as $index => $group) {
+      $tableData[] = [
+        'idG' . ($index + 1) => $group->id,
+        'modDep' . ($index + 1) => $group->sports_modality,
+        'DLUN' . ($index + 1) => $this->getScheduleTime($group, 'DLUN'),
+        'SHDLUST' . ($index + 1) => $this->getScheduleTime($group, 'SHDLUST'),
+        'DMAR' . ($index + 1) => $this->getScheduleTime($group, 'DMAR'),
+        'SHDMART' . ($index + 1) => $this->getScheduleTime($group, 'SHDMART'),
+        'DMIE' . ($index + 1) => $this->getScheduleTime($group, 'DMIE'),
+        'SHDMIEST' . ($index + 1) => $this->getScheduleTime($group, 'SHDMIEST'),
+        'DJUE' . ($index + 1) => $this->getScheduleTime($group, 'DJUE'),
+        'SHDJUEST' . ($index + 1) => $this->getScheduleTime($group, 'SHDJUEST'),
+        'DVIE' . ($index + 1) => $this->getScheduleTime($group, 'DVIE'),
+        'SHDVIEST' . ($index + 1) => $this->getScheduleTime($group, 'SHDVIEST'),
+        'DSABN' . ($index + 1) => $this->getScheduleTime($group, 'DSABN'),
+        'SHDSABST' . ($index + 1) => $this->getScheduleTime($group, 'SHDSABST'),
+        'GNEDP' . ($index + 1) => $group->main_sports_stage_name,
+        'GDEDP' . ($index + 1) => $group->main_sports_stage_address,
+        'GDEDA' . ($index + 1) => $group->alt_sports_stage_address,
+      ];
     }
 
-    public function generateDoc($id)
-    {
-        $ChronogramReport = $this->cronogram->findOrFail($id);
-        $grupo = $ChronogramReport->load('groups');
+    $templateProcessor->cloneRowAndSetValues('idG1', $tableData);
+    $templateProcessor->saveAs($outputPath);
 
-        $templatePath = public_path('Template/Chronogram/template.docx');
-        $outputPath = public_path('Template/Chronogram/template/Chronograma_' . $id . '.docx');
+    $relativePath = 'Template/Chronogram/template/Chronograma_' . $id . '.docx';
+    return $relativePath;
+  }
 
-        $templateProcessor = new TemplateProcessor($templatePath);
-
-        $data = [
-            'update_date' => $grupo->updated_at,
-            'MONTH' => $grupo->mes->name,
-            'MONITOR_name' => $grupo->creator->name,
-            'MONITOR_lastname' => $grupo->creator->lastname,
-            'zone' => $grupo->zone->name,
-            'municipio' => $grupo->municipio->name,
-        ];
-
-        $templateProcessor->setValues($data);
-
-        try {
-            $templateProcessor->setImageValue('imagen', [
-                'path' => storage_path("app/public/" . $ChronogramReport->file),
-                'width' => 500,
-                'height' => 500,
-                'ratio' => false,
-            ]);
-        } catch (\Exception $e) {
-        }
-
-        // Exportar información de los grupos
-        $groupsData = '';
-        foreach ($grupo->groups as $group) {
-            $groupsData .= "ID: " . $group->id . "\n";
-            $groupsData .= "Sports Modality: " . $group->sports_modality . "\n";
-            $groupsData .= "Main Sports Stage Name: " . $group->main_sports_stage_name . "\n";
-            $groupsData .= "Main Sports Stage Address: " . $group->main_sports_stage_address . "\n";
-            $groupsData .= "Alternative Sports Stage Name: " . $group->alt_sports_stage_name . "\n";
-            $groupsData .= "Alternative Sports Stage Address: " . $group->alt_sports_stage_address . "\n";
-            $groupsData .= "Schedules: \n";
-            foreach ($group->schedules as $schedule) {
-                $groupsData .= "- Day: " . $schedule->day . ", Start Time: " . $schedule->start_time . ", End Time: " . $schedule->end_time . "\n";
-            }
-            $groupsData .= "\n";
-        }
-        $templateProcessor->setValue('groups_data', $groupsData);
-
-        $templateProcessor->saveAs($outputPath);
-
-        $relativePath = 'Template/Chronogram/template/Chronograma_' . $id . '.docx';
-        return $relativePath;
+  private function getScheduleTime($group, $day)
+  {
+    $schedules = $group->schedules;
+    foreach ($schedules as $schedule) {
+      if ($schedule->day === $day) {
+        return $schedule->start_time . ' - ' . $schedule->end_time;
+      }
     }
+    return '';
+  }
 }
 
