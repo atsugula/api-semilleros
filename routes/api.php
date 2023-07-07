e4f12cdcfec18ceca99bc4145224192e1817a2b9
zarrok <anakinvv@gmail.com>
2023-07-06 23:15:04 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -275,8 +275,6 @@ Route::middleware(['auth:sanctum', 'verified', 'logNavigationHistory', 'verifyUs
     // Ruta Todos los usuarios que pertenezcan a la region del usuario logueado
     Route::get('userlogin/users/regions', [UserController::class, 'getUserLoginUsersRegions']);
 
-    
-
  });
 
 /* RUTAS DE PRUEBA JORGE */
