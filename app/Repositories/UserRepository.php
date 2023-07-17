6eba0c4c7180b6e996b1a82ec894cc1272aa91e8
DiegoAlejandroGarzon <tingonay10@hotmail.com>
2023-07-17 11:36:56 -0500

    SSS69 datatable usuarios role coordinador maritimo

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -53,7 +53,7 @@ class UserRepository
         if ($rol_id == config('roles.metodologo')){
             $query = $query->where('methodology_id', Auth::user()->id);
         }
-        if(in_array($rol_id, [config('roles.coordinador_regional'), config('roles.director_programa'), config('roles.subdirector_tecnico'), config('roles.psicologo')])){
+        if(in_array($rol_id, [config('roles.coordinador_regional'), config('roles.director_programa'), config('roles.subdirector_tecnico'), config('roles.psicologo'), config('roles.coordinador_maritimo')])){
             $query = $query->where('manager_id', Auth::user()->id);
         }
         if(in_array($rol_id, [config('roles.coordinador_psicosocial')])){
