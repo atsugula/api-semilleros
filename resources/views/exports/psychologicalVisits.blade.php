ec4a504b78c9a0984397f29130d182cc666168e4
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2023-07-24 15:44:57 -0500

    correccion visita pshicosocial

--- resources/views/exports/psychologicalVisits.blade.php
+++ resources/views/exports/psychologicalVisits.blade.php
@@ -23,6 +23,27 @@
                 <b>DISIPLINA</b>
             </th>
             <th style="width: 30px;text-align:center">
+                <b>ESCENARIO DEPORTIVO</b>
+            </th>
+            <th style="width: 30px;text-align:center">
+                <b>OBJETIVO DEL ACOMPAÑAMIENTO</b>
+            </th>
+            <th style="width: 30px;text-align:center">
+                <b>RECONOCIMIENTO DE NOMBRE DE PROYECTO</b>
+            </th>
+            <th style="width: 30px;text-align:center">
+                <b>RECONOCIMIENTO DE VALOR</b>
+            </th>
+            <th style="width: 30px;text-align:center">
+                <b>SE OBSERVA ORGANIZACIÓN, DISCIPLINA Y BUEN MANEJO</b>
+            </th>
+            <th style="width: 30px;text-align:center">
+                <b>DESCRIPCIÓN DE ACTIVIDADES</b>
+            </th>
+            <th style="width: 30px;text-align:center">
+                <b>OBSERVACIONES</b>
+            </th>
+            <th style="width: 30px;text-align:center">
                 <b>ESTADO</b>
             </th>
             <th style="width: 30px;text-align:center">
@@ -40,6 +61,46 @@
                 <td>{{ $value->date_visit }}</td>
                 <td>{{ $value->number_beneficiaries }}</td>
                 <td>{{ $value->dicipline }}</td>
+                <td>{{ $value->scenery }}</td>
+                <td>{{ $value->objetive }}</td>
+                @php
+                    $documentTypes = [
+                        "true" => "Verdadero",
+                        "false" => "Falso ",
+                    ];
+                    $documentType = $value->beneficiaries_recognize_name;
+                @endphp
+                @if(isset($documentTypes[$documentType]))
+                    <td>{{ $documentTypes[$documentType] }}</td>
+                @else
+                    <td>Desconocido</td>
+                @endif
+                @php
+                    $documentTypes = [
+                        "true" => "Verdadero",
+                        "false" => "Falso ",
+                    ];
+                    $documentType = $value->beneficiary_recognize_value;
+                @endphp
+                @if(isset($documentTypes[$documentType]))
+                    <td>{{ $documentTypes[$documentType] }}</td>
+                @else
+                    <td>Desconocido</td>
+                @endif
+                @php
+                    $documentTypes = [
+                        "true" => "Verdadero",
+                        "false" => "Falso ",
+                    ];
+                    $documentType = $value->all_ok;
+                @endphp
+                @if(isset($documentTypes[$documentType]))
+                    <td>{{ $documentTypes[$documentType] }}</td>
+                @else
+                    <td>Desconocido</td>
+                @endif
+                <td>{{ $value->description }}</td>
+                <td>{{ $value->observations }}</td>
                 <td>{{ $value->status }}</td>
                 <td>{{ $value->created_at }}</td>
             </tr>
